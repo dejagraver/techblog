7aacdb53792fff863f7fb457810aaee3867d7e84
dejagraver <dejaamaartin@gmail.com>
2021-07-21 13:37:17 -0400

    create post/delete post trouble shooting

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,20 @@
+async function deletePostHandler(event) {
+  event.preventDefault();
+  const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch('/api/posts/' + post_id, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#delete-btn').addEventListener('click', deletePostHandler);
